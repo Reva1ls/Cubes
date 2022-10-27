d3c32521dfb9248d8f2f773816613c21c57dd9c0
VladDenisov <115172631+DruVodVkus@users.noreply.github.com>
2022-10-27 14:10:15 +0300

    Add files via upload

---
+++ CubesConsole/CubesConsole/CubesConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿double balance = 1000;
+Console.WriteLine("Balance is " + balance);
+do
+{
+
+    Console.WriteLine("Write a bet");
+    double bet = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Write number from 1 to 6 ");
+    double a = Convert.ToDouble(Console.ReadLine());// 
+    Random rnd = new Random();
+    int value = rnd.Next(1, 6);
+    Console.WriteLine("Number on cube 1: " + value);
+    Random r2d = new Random();
+    int value2 = r2d.Next(1, 6);
+    Console.WriteLine("Number on cube 2: " + value2);
+
+    Console.WriteLine();
+    double rand = value;
+    double ra2d = value2;
+    if ((a == rand) | (a == ra2d))
+    {
+        Console.WriteLine("Congratulations you won the jackpot");
+        balance = balance + (bet * a);
+    }
+    else
+    {
+        {
+            if ((rand + ra2d) % 2 == 0)
+            {
+                balance = balance + bet;
+            }
+            else
+            {
+                balance = balance - bet;
+            }
+        }
+    }
+    Console.WriteLine("Balance is " + balance);
+} while (balance > 0);
+Console.WriteLine("You lose :(");
